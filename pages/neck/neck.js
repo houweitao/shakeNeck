c1d567a94ace83f4299165d0057bfdf322face24
weitao <weitao@Harshal-001.fareast.corp.microsoft.com>
2023-03-07 02:10:25 +0800

    add neck page

---
+++ pages/neck/neck.js
@@ -0,0 +1,53 @@
+Page({
+  data: {
+    texts: ["Text 1", "Text 2", "Text 3skkkkkkkkk", "Text 4"],
+    joy:""
+  },
+
+  onLoad: function () {
+    var that = this//不要漏了这句，很重要
+    that.changeText()
+  },
+
+
+  changeText: function() {
+    var that = this//不要漏了这句，很重要
+    console.log("reauest story data ");
+    wx.request({
+      url: 'https://interface.meiriyiwen.com/article/random?dev=1',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        console.log("neck story.data: ");
+        console.log(res.data);
+        console.log("neck story.data.digest: ");
+        console.log(res.data.data.digest);
+        //将获取到的json数据，存在名字叫zhihu的这个数组中
+        that.setData({
+            joy: res.data.data.digest,
+          //res代表success函数的事件对，data是固定的，stories是是上面json数据中stories
+        })
+      }
+    })
+
+    var size = that.data.joy.length
+
+    const chunkSize = Math.ceil(size / 4); // 计算每个子字符串的长度
+
+    // 切割成四个不同的子字符串
+    const chunk1 = that.data.joy.slice(0, chunkSize);
+    const chunk2 = that.data.joy.slice(chunkSize, chunkSize * 2);
+    const chunk3 = that.data.joy.slice(chunkSize * 2, chunkSize * 3);
+    const chunk4 = that.data.joy.slice(chunkSize * 3, size);
+
+    this.setData({
+        'texts[0]': chunk1,
+        'texts[1]': chunk3,
+        // 'texts[2]': chunk2,
+        'texts[2]': "chunk2kaflasfaksfkasflaflaflalfl",
+        'texts[3]': chunk4
+      })
+
+  }
+})
